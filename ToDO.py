7bbb178907be1b9461c7b1ee945e0ad246a805e9
jtavara23 <joshuatavara23@gmail.com>
2018-09-06 09:41:43 -0500

    toDo updated

---
+++ ToDO.py
@@ -0,0 +1,21 @@
+'models/./model-9450'
+../signals_database/traffic-signs-data/test_2Processed.p
+Acierto en el conjunto de Testing: 98.50% (12441 / 12630)
+2018-09-04
+-------------------------------------------------------------
+'models/./model-11340'
+../signals_database/traffic-signs-data/test_2Processed.p
+Acierto en el conjunto de Testing: 98.50% (12441 / 12630)
+2018-09-04
+
+--------
+(1)
+FOR models
+Same  result 
+So, what needs to be prove is to 
+clone models folder and named it as "models_25-5_30-1"
+and change LR to be 1e-4, and  ITERACIONES_ENTRENAMIENTO = ITER_PER_EPOCA * 30
+--------
+(2)
+FOR models10extend
+continue with the same LR, till 20 epocs.
