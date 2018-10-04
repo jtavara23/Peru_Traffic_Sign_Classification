2e436d4ff6a7d9b6497a5ddc560d635d10bac357
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2018-10-03 22:48:32 -0500

    delete managedDATA files

--- manageData/convertPickle.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import numpy
-#with open("traffic-signs-data/train.p", "rb") as f:
-with open("traffic-signs-data/test.p", "rb") as f:
-    w = pickle.load(f)
-
-#pickle.dump(w, open("traffic-signs-data/trainData.p","wb"), protocol=2)
-pickle.dump(w, open("traffic-signs-data/testData.p","wb"), protocol=2)
